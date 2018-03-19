b6f524a4d538a93c1cc8bc7387e628d1000caf5f
Sergey Korchinskiy <k.serjey@gmail.com>
2018-03-19 12:23:14 +0300

    Initi commit

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: pkg.main,
+    format: 'cjs'
+  },
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    uglify()
+  ]
+}
