81c035f1ec748631f2980e79c436bf8090c30c63
Sergey Korchinskiy <k.serjey@gmail.com>
2018-03-20 03:17:58 +0300

    HoC that creates RequestComponent

---
+++ src/createRequest.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallowEqual, omit } from './utils';
+
+function createRequest(initialValue, request) {
+  return class RequestComponent extends React.Component {
+    state = {
+      isLoading: true,
+      data: initialValue,
+      error: false
+    };
+
+    componentDidMount() {
+      this.fetchData();
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const nextArgs = omit(nextProps, ["render"]);
+      const currentArgs = omit(this.props, ["render"]);
+
+      if (!shallowEqual(currentArgs, nextArgs)) {
+        this.shouldUpdate = true;
+        this.setState({ isLoading: true });
+      }
+    }
+
+    componentDidUpdate() {
+      if (this.shouldUpdate) {
+        this.shouldUpdate = false;
+        this.fetchData();
+      }
+    }
+
+    latestRequestId = 0;
+
+    fetchData = () => {
+      this.latestRequestId += 1;
+      const requestId = this.latestRequestId;
+      const args = omit(this.props, ["render"]);
+
+      request(args).then(
+        data => {
+          if (this.latestRequestId === requestId) {
+            this.setState({ data, isLoading: false, error: false });
+          }
+        },
+        error => {
+          if (this.latestRequestId === requestId) {
+            this.setState({ data: error, isLoading: false, error: true });
+          }
+        }
+      );
+    };
+
+    render() {
+      return this.props.render(this.state);
+    }
+  };
+}
+
+export default createRequest;
---
+++ src/utils.js
@@ -0,0 +1,20 @@
+export function shallowEqual(objectA, objectB) {
+  if (objectA === objectB) return true;
+
+  const keysOfA = Object.keys(objectA);
+  const keysOfB = Object.keys(objectB);
+
+  if (keysOfA.length !== keysOfB.length) return false;
+
+  for (let index = 0; index < keysOfA.length; index += 1) {
+    const key = keysOfA[index];
+    if (objectA[key] !== objectB[key]) return false;
+  }
+
+  return true;
+}
+
+export function omit(object, props) {
+  const keys = Object.keys(object).filter(key => props.indexOf(key) < 0);
+  return keys.reduce((acc, key) => ({ ...acc, [key]: object[key] }), {});
+}
